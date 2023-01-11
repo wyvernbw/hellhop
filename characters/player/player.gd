073f3806eb5554d9e15a0a25ed67cf2f07c8a546
wyvernbw <iamwyvern511@gmail.com>
2023-01-11 19:43:59 +0200

    basic player script

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,31 +1,27 @@
 extends CharacterBody3D
 
+const MAX_ANGLE = 90.0
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var mouse_sensitivity = 0.01
+@export var speed: float = 10.0 # m/s
+@export var jump_velocity: float = 10.0 # m/s
+
+@onready var _head = $Head
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		_head.rotate_x(-event.relative.y * mouse_sensitivity)
+		_head.rotation.x = clamp(_head.rotation.x, deg_to_rad(-MAX_ANGLE), deg_to_rad(MAX_ANGLE))
